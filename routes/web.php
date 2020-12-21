8b85da990a6d9bda77a03e759b71f8a3e4f4216a
Marcelo Morais <morais.marcelo@outlook.com>
2020-12-21 19:39:32 -0300

    setando ambient

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/produtos', 'ProductController@index')->name('produtos.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
